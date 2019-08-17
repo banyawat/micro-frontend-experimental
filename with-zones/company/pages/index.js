2704b2020f77d0ffcbbbde7ff1963a677f308988
Teusday <bunyawat.38@gmail.com>
2019-08-17 18:06:34 +0700

    completely-zoning

---
+++ with-zones/company/pages/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Companies extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Company</h2>
+      </div>
+    )
+  }
+}
---
+++ with-zones/resumes/pages/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class index extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Searching for any resumes?</h1>
+      </div>
+    )
+  }
+}
