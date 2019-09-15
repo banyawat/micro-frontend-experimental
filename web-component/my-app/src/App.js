6c16fb6e81b16fd00716839e99824da257c26994
chaowakrit <chaowakrit@thinknet.co.th>
2019-09-15 15:48:24 +0700

    initial micro component login

--- web-component/my-app/src/App.js
+++ web-component/my-app/src/App.js
@@ -10,6 +10,9 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <react-el style={{
+          width:300
+        }}></react-el>
         <a
           className="App-link"
           href="https://reactjs.org"
