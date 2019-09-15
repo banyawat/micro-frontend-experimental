5500debbc248e7dc939ddafb9dc7266d93d943d5
Banyawat Kaewsamer <banyawat@thinknet.co.th>
2019-09-15 15:14:11 +0700

    add script to run

--- netflux/react-wrapper/src/App.js
+++ netflux/react-wrapper/src/App.js
@@ -5,14 +5,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/zone.js/0.9.1/zone.min.js"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/webcomponentsjs/2.2.10/custom-elements-es5-adapter.js"></script>
-        <script src="http://localhost:5002/main.js"></script>
-      </header>
       <Navbar />
       <div className="contatiner">
-        test
+        <thumbnail-el></thumbnail-el>
       </div>
     </div>
   );
--- netflux/thumbnail/src/index.js
+++ netflux/thumbnail/src/index.js
@@ -7,7 +7,7 @@ class ThumbnailRenderer extends HTMLElement {
     this.attachShadow({ mode: 'open' }).appendChild(mountPoint);
 
     const url = 'https://www.google.com/search?q='
-    ReactDOM.render(<a href={url}>test</a>, mountPoint);
+    ReactDOM.render(<a href={url}>PLEASE SHOW ME</a>, mountPoint);
   }
 }
 customElements.define('thumbnail-el', ThumbnailRenderer)
