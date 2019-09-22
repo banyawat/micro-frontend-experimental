bafb817a65a678973fd8df3b1110028e0d08685e
Banyawat Kaewsamer <banyawat@thinknet.co.th>
2019-09-22 15:04:48 +0700

    update css for container

--- netflux/react-wrapper/src/Layout/index.js
+++ netflux/react-wrapper/src/Layout/index.js
@@ -4,7 +4,9 @@ import NavBar from '../components/NavBar'
 const Layout = ({ children }) => (
   <div className="main-layout">
     <NavBar />
-    {children}
+    <div className="container">
+      {children}
+    </div>
   </div>
 )
 
