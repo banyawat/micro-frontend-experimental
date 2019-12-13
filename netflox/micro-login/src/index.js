a89267b9db601cb41319f45e677c6847f1bfc524
chaowakrit <chaowakrit@thinknet.co.th>
2019-12-13 21:57:51 +0700

    refactor folder name and how to dispatch event

--- netflox/micro-login/src/index.js
+++ netflox/micro-login/src/index.js
@@ -1,78 +1,17 @@
 import * as React from 'react';
-import { render, unmountComponentAtNode } from 'react-dom';
-import htmlToReact from 'html-to-react';
-import PropTypes from 'prop-types';
+import ReactDOM from 'react-dom'
 
 import LoginComponent from './Login'
 
 class MicroLogin extends HTMLElement {
-  
   connectedCallback() {
-    this._innerHTML = this.innerHTML;
-    this.mount();
-  }
-
-  disconnectedCallback() {
-    this.unmount();
-  }
-
-  update() {
-    this.unmount();
-    this.mount();
-  }
-
-  mount() {
-    const propTypes = LoginComponent.propTypes ? LoginComponent.propTypes : {};
-    const events = LoginComponent.propTypes ? LoginComponent.propTypes : {};
+    ReactDOM.render(<LoginComponent />, this);
     const props = {
-      ...this.getProps(this.attributes, propTypes),
-      ...this.getEvents(events),
-      children: this.parseHtmlToReact(this.innerHTML)
-    };
+      onLogin: args => 
+        this.dispatchEvent(new CustomEvent('onLogin', {detail:args}))
+    }
 
-    render(<LoginComponent {...props}/>, this);
-  }
-
-  unmount() {
-    unmountComponentAtNode(this);
-  }
-
-  parseHtmlToReact(html) {
-    return html && new htmlToReact.Parser().parse(html);
-  }
-
-  getProps(attributes, propTypes) {
-    propTypes = propTypes|| {};
-    return [ ...attributes ]         
-      .filter(attr => attr.name !== 'style')         
-      .map(attr => this.convert(propTypes, attr.name, attr.value))
-      .reduce((props, prop) => 
-        ({ ...props, [prop.name]: prop.value }), {});
-  }
-
-  getEvents(events) {
-    return Object.keys(events)
-      .filter(key => events[key] === PropTypes.func)
-      .reduce((prev, curr) => ({
-        ...prev,
-        [curr]: args => this.dispatchEvent(new CustomEvent(curr, {detail:{...args}}))
-      }), {});
-  }
-
-  convert(propTypes, attrName, attrValue) {
-    const propName = Object.keys(propTypes)
-      .find(key => key.toLowerCase() == attrName);
-    let value = attrValue;
-    if (attrValue === 'true' || attrValue === 'false') 
-      value = attrValue == 'true'; 
-    else if (!isNaN(attrValue) && attrValue !== '') 
-      value = +attrValue; 
-    else if (/^{.*}/.exec(attrValue)) 
-      value = JSON.parse(attrValue);
-    return {         
-      name: propName ? propName : attrName,         
-      value: value      
-    };
+    ReactDOM.render(<LoginComponent {...props} />, this);
   }
 }
 customElements.define('micro-login', MicroLogin);
--- netflox/wrapper/src/screens/Login.js
+++ netflox/wrapper/src/screens/Login.js
@@ -1,14 +1,20 @@
 import React from 'react'
 import { withRouter } from 'react-router-dom'
 import MainLayout from '../Layout/MainLayout'
-import MicroWrapper from '../components/MicroWrapper'
 
 class Login extends React.Component {
-  state = {
-  }
 
   constructor(props) {
     super(props)
+    this.microRef = React.createRef();
+  }
+
+  componentDidMount() {
+    this.microRef.current.addEventListener('onLogin', this.onLogin);
+  }
+  
+  componentWillUnmount() {
+    this.microRef.current.removeEventListener('onLogin', this.onLogin);
   }
 
   onLogin = (event) => {
@@ -23,9 +29,7 @@ class Login extends React.Component {
     return (
       <div className="login">
         <MainLayout>
-          <MicroWrapper test1='test1' test2={2} test3={true} onLogin={this.onLogin}>
-            <micro-login/>
-          </MicroWrapper>
+            <micro-login ref={this.microRef}/>
         </MainLayout>
       </div>
     )
